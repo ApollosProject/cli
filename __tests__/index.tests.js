6e535197c98cd1efb02571c4d7ebb30253d13ce5
Michael Neeley <micneeley14@gmail.com>
2022-06-03 00:03:22 -0400

    adds version snap to release

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -3,6 +3,6 @@ const { exec } = require('child_process');
 test('gets the version', (done) => {
   exec('node index.js -V', (err, out) => {
     expect(out.trim()).toMatchSnapshot();
-    done();
+    done(err);
   });
 });
