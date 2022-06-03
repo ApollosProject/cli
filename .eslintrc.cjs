34b2034345deece155b9e052861f4e36637d992e
Michael Neeley <micneeley14@gmail.com>
2022-06-03 00:32:32 -0400

    chore: adds linting and tests (#3)
    * chore: adds linting and tests
    
    * fix workflow
    
    * yarn install
    
    * adds version snap to release
    
    * fix secrets

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,10 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: ['airbnb-base'],
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
