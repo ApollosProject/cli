2e85c1eea065b43da5b2c4356cfee4c1510fd97a
Vincent Wilson <vince@classyh.at>
2022-06-08 15:28:37 -0400

    Include stderr with any google deploys

--- commands/mobile/deploy.js
+++ commands/mobile/deploy.js
@@ -173,6 +173,7 @@ export default () => {
       } catch (e) {
         spinner.fail('Failed');
         consola.log(e.stdout);
+        consola.log(e.stderr);
         process.exit(1);
       }
       spinner.succeed('Deployed!');
