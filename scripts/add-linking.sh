1eae485283dff043ca8a638f374092a87ec7cf3e
Michael Neeley <micneeley14@gmail.com>
2022-06-14 19:13:02 -0400

    feat: adds universal linking

---
+++ scripts/add-linking.sh
@@ -0,0 +1,10 @@
+#UNIVERSAL_LINK_HOST=<API Heroku Url>
+#APPLE_APP_ID=<com.church.app>
+#APPLE_TEAM_ID=<XXXXXXX>
+#IOS_REDIRECT=<url of app in App store>
+#ANDROID_REDIRECT=<url of app in Google Play store>
+#GOOGLE_APP_ID=<com.church.app>
+#GOOGLE_KEYSTORE_SHA256=<XX:XX:XX:XX>
+$KEY=$1
+
+$UNIVERSAL_LINK_HOST=$(
